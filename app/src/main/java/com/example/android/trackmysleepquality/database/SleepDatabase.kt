70e33b378f6a2da53d4b157c733363b1eec1f59c
maria-alane <maria.alane.bs@gmail.com>
2022-01-06 14:53:34 -0300

    Creating a Room Database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,41 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDataBase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: SleepDataBase? = null
+
+        fun getInstance(context: Context) : SleepDataBase {
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDataBase::class.java,
+                        "sleep_history_database"
+                    )
+
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+
+}
